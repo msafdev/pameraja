6efd1b7f6c60212472e448fa55c9b17a5002e0da
salman261002 <salmanalfarisi261002@gmail.com>
2024-05-31 19:36:04 +0700

    add loading state

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,6 +1,4 @@
-import { createClient } from "@/utils/supabase/server";
-
-import Post from "@/components/main/post";
+import PostContainer from "@/components/main/post-container";
 
 export default async function Page({
   searchParams,
@@ -8,56 +6,10 @@ export default async function Page({
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
   const query = searchParams.channel ? searchParams.channel : "all";
-  const supabase = createClient();
-  let posts = [];
-  let error = null;
-
-  if (query === "all") {
-    const { data, error: fetchError } = await supabase
-      .from("post")
-      .select("*")
-      .order("created_at", { ascending: false });
-    posts = data || [];
-    error = fetchError;
-  } else if (query === "hot") {
-    const { data, error: fetchError } = await supabase
-      .from("post")
-      .select("*")
-      .order("upvoted", { ascending: true });
-    posts = data || [];
-    error = fetchError;
-  } else if (query === "old") {
-    const { data, error: fetchError } = await supabase
-      .from("post")
-      .select("*")
-      .order("created_at", { ascending: true });
-    posts = data || [];
-    error = fetchError;
-  } else {
-    const { data, error: fetchError } = await supabase
-      .from("post")
-      .select("*")
-      .like("channel", `%${query}%` as string)
-      .order("created_at", { ascending: false });
-    posts = data || [];
-    error = fetchError;
-  }
 
   return (
-    <div className="flex flex-col gap-y-4 px-4 md:px-6 lg:px-8 py-4 md:py-6 lg:py-8">
-      {posts &&
-        posts.map((post, index) => (
-          <Post
-            key={index}
-            id={post.post_id}
-            title={post.title}
-            link={post.link}
-            author_id={post.author_id}
-            created_at={post.created_at}
-            upvoted={post.upvoted || []}
-            channel={post.channel}
-          />
-        ))}
+    <div className="px-4 md:px-6 lg:px-8 py-4 md:py-6 lg:py-8">
+      <PostContainer query={query} />
     </div>
   );
 }
---
+++ components/main/post-container.tsx
@@ -0,0 +1,61 @@
+import React, { Suspense } from "react";
+
+import { createClient } from "@/utils/supabase/server";
+import Post from "./post";
+
+const PostContainer = async ({ query }: { query: string | string[] }) => {
+  const supabase = createClient();
+  let posts = [];
+  let error = null;
+
+  if (query === "all") {
+    const { data, error: fetchError } = await supabase
+      .from("post")
+      .select("*")
+      .order("created_at", { ascending: false });
+    posts = data || [];
+    error = fetchError;
+  } else if (query === "hot") {
+    const { data, error: fetchError } = await supabase
+      .from("post")
+      .select("*")
+      .order("upvoted", { ascending: true });
+    posts = data || [];
+    error = fetchError;
+  } else if (query === "old") {
+    const { data, error: fetchError } = await supabase
+      .from("post")
+      .select("*")
+      .order("created_at", { ascending: true });
+    posts = data || [];
+    error = fetchError;
+  } else {
+    const { data, error: fetchError } = await supabase
+      .from("post")
+      .select("*")
+      .like("channel", `%${query}%` as string)
+      .order("created_at", { ascending: false });
+    posts = data || [];
+    error = fetchError;
+  }
+
+  return (
+    <div className="flex flex-col gap-y-4 w-full">
+      {posts &&
+        posts.map((post, index) => (
+          <Post
+            key={index}
+            id={post.post_id}
+            title={post.title}
+            link={post.link}
+            author_id={post.author_id}
+            created_at={post.created_at}
+            upvoted={post.upvoted || []}
+            channel={post.channel}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default PostContainer;
